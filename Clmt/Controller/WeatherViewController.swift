ab178ed39c85fba15a6e65af7b67c76dd5ec99d9
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-17 10:46:09 -0600

    Define UI layout
    Link IBActions/IBOutlets to WeatherViewController. Extend UITextFieldDelegate to manage text validation and post submission.
    
    Related to: none

--- Clmt/Controller/WeatherViewController.swift
+++ Clmt/Controller/WeatherViewController.swift
@@ -8,17 +8,46 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var search: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        search.delegate = self // delegates used to notify other elements (eg, user end editing..)
     }
 
-
+    @IBAction func search(_ sender: UIButton) {
+        // send api request to weather api
+        print(search.text!)
+        search.endEditing(true)
+    }
+    
+    // MARK: Delegates
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // send api request to weather api ...
+        print(search.text!)
+        textField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        // perform some validation here
+        if textField.text != "" {
+            return true
+        }
+        textField.placeholder = "Type something here, buddy!"
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // send api request to weather api
+        search.text = "" // reset the text field after submission
+    }
 }
 
