9a7a1b83981a446e7bbc71d431c315de466ff9ef
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-16 21:32:29 -0600

    Boilerplate Xcode/Swift Code
    Commit boilerplate code. Define a few IBActions/Outlets (no functionality at this point)
    
    Related to: none

---
+++ Clmt/Controller/WeatherViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherViewController.swift
+//  Clmt
+//
+//  Created by Darren Rambaud on 02/16/2020.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
